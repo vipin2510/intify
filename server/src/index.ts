ce6dcb8dc88726abe560a6b0ead3ea0729241a8f
Raja Singh <rajasgh18@gmail.com>
2024-05-15 12:41:49 +0530

    Created route for spreadsheet fetching

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import spreadsheetRoute from './routes/spreadsheet';
 
 const App = express();
 
@@ -6,7 +7,9 @@ App.use(express.json());
 
 App.get('/', (req: Request, res: Response) => {
     res.status(200).send("Server Running correctly");
-})
+});
+
+App.use("/api", spreadsheetRoute);
 
 App.listen(5000, () => {
     console.log("Server running at http://localhost:5000");
