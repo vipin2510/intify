35887c0d84b9cf4c5579b22dfaa4ea81b8b03e40
Raja Singh <rajasgh18@gmail.com>
2024-05-15 13:20:25 +0530

    Api key and spreadsheet id now come from .env

--- server/src/controller/spreadsheet-controller.ts
+++ server/src/controller/spreadsheet-controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 import { google } from 'googleapis';
-const apiKey = 'AIzaSyCxrvxY0Nmf5txZwCIuTBWEEIw7HVC8bro';
+
+const apiKey = process.env.API_KEY;
+
 
 export const getSpreadsheetData = async (req: Request, res: Response) => {
     const sheets = google.sheets({ version: 'v4', auth: apiKey });
-
+    
     // The ID and range of the spreadsheet
-    const spreadsheetId = '1u0wz3jnjb50T_6D8YCqHnpaT7Cejyg5Rafu7FBA1vZk';
+    const spreadsheetId = process.env.SPREADSHEET_ID;
     const range = 'Int Main Sheet!A1:Z'; // Adjust the range as needed
+    console.log(apiKey, spreadsheetId)
 
     try {
         const response = await sheets.spreadsheets.values.get({
