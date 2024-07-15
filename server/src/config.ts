cd86426fc85c7a2b5f63a9d3fbb236a0be184928
vipin2510 <77065205+vipin2510@users.noreply.github.com>
2024-07-15 16:20:13 +0530

    added mail
    mail of authorised people added

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,9 +3,15 @@
 export const AUTH_CONFIG = {
     ALLOWED_EMAILS: [
       "vipingautamooooo16@gmail.com",
-      "",
-      "test@example.com"
-      // Add more authorized email addresses here
+      "arvindkishor76@gmail.com","monikamaravi989@gmail.com","ritesh.dsp08@gmail.com","abhishekpainkra@gmail.com"
+      ,"asharani92.uranw@gmail.com","rdchandra81.dc@gmail.com","satyasingh8653@gmail.com","Janaksahu2013@gmail.com","test@example.com",
+      "sudarshansinghdhruw70@gmail.com","shivkumarabhanpur2903@gmail.com","jaiksahu80@gmail.com"
+        ,"rngautambablu@gmail.com","krishnaksahu05@gmail.com","Bheeshenpisda1981@mail.com","grogarey29@gmail.com"
+        ,"sureshbhilaicg@gmail.com","riteshyadav.its@gmail.com","abhishekpainkra@gmail.com"
+        ,"baghelh6@gmail.com","lawkeshbansal123@gmail.com","ashishd4457@gmail.com","manojkr.mandavi@gmail.com","prashant2687dew@gmail.com"
+        ,"guriarobinson9@gmail.com","O8vinaysahu@gmail/","prabhat140387@gmail.com","opscellnpr@gmail.com","rajasgh18@gmail.com",
+        "prathamsahu2809@gmail.com"
+        // Add more authorized email addresses here
     ],
     GOOGLE_CLIENT_ID: "31972503524-c5ao35e1arcjb53ruskm8cpr36gm4dg5.apps.googleusercontent.com"
   };
