050f11132e7f4fd538c3b209980f6993501ade50
vipin2510 <77065205+vipin2510@users.noreply.github.com>
2025-07-01 21:20:17 +0530

    Update spreadsheet.ts

--- server/src/routes/spreadsheet.ts
+++ server/src/routes/spreadsheet.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/spreadsheet', getSpreadsheetData)
 router.get('/spreadsheet-sukma', getSpreadsheetDataSukma)
-router.get('/spreadsheet-bijapur', getSpreadsheetDataBijapur)
+router.get('/spreadsheet-bijapur', getSpreadsheetData)
 
 
 export default router;
