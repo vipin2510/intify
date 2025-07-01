6d282cfd25bd6ff4d7902b0af1270b1a76e83300
vipin2510 <77065205+vipin2510@users.noreply.github.com>
2025-07-01 21:11:35 +0530

    Update env.d.ts

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJs{
     interface ProcessEnv{
         SPREADSHEET_ID: string;
         SPREADSHEET_ID_SUKMA: string;
+         SPREADSHEET_ID_BIJAPUR: string;
         API_KEY: string;
     }
 }
